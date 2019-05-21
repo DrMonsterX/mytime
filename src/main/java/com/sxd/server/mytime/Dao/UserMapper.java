59e68b01aeb6d3178a6b5a63d1e9e375a771a399
Sxd <714599564@qq.com>
2019-05-21 10:40:16 +0800

    add user entity

--- src/main/java/com/sxd/server/mytime/Dao/UserMapper.java
+++ src/main/java/com/sxd/server/mytime/Dao/UserMapper.java
@@ -1,4 +1,7 @@
 package com.sxd.server.mytime.Dao;
 
-public interface UserMapper {
+import com.sxd.server.mytime.Entity.User;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UserMapper extends Mapper<User> {
 }
--- src/main/java/com/sxd/server/mytime/Entity/User.java
+++ src/main/java/com/sxd/server/mytime/Entity/User.java
@@ -1,4 +1,64 @@
 package com.sxd.server.mytime.Entity;
 
+import javax.persistence.*;
+
+@Table(name = "user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY,generator = "JDBC")
+    @Column(name = "user_id")
+    private int userId;
+
+    @Column(name = "user_name")
+    private String name;
+
+    @Column(name = "icon_id")
+    private int iconId;
+
+    @Column(name = "user_password")
+    private String password;
+
+    @Column(name = "completeness_id")
+    private int completenessId;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getIconId() {
+        return iconId;
+    }
+
+    public void setIconId(int iconId) {
+        this.iconId = iconId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getCompletenessId() {
+        return completenessId;
+    }
+
+    public void setCompletenessId(int completenessId) {
+        this.completenessId = completenessId;
+    }
 }
