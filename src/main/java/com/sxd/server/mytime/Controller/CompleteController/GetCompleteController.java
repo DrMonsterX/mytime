51dc84b566348b7134ee44d3d7019215a292d650
Sxd <714599564@qq.com>
2019-05-28 08:41:50 +0800

    rename conpleteness controller

--- src/main/java/com/sxd/server/mytime/Controller/CompleteController/GetCompleteController.java
+++ src/main/java/com/sxd/server/mytime/Controller/CompleteController/GetCompleteController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GetCompleteController implements IGetCompleteController{
+public class GetCompleteController implements IGetComplete {
     @Autowired
     private GetCompleteService getCompleteService;
 
--- src/main/java/com/sxd/server/mytime/Controller/CompleteController/IGetComplete.java
+++ src/main/java/com/sxd/server/mytime/Controller/CompleteController/IGetComplete.java
@@ -1,6 +1,6 @@
 package com.sxd.server.mytime.Controller.CompleteController;
 
-public interface IGetCompleteController {
+public interface IGetComplete {
     //根据用户id获取用户本周完成度数据
     public int getWeekComplete(Integer userId);
 }
