55007438a57fd505dc406a8bb8a59286cbdfc95c
Sxd <714599564@qq.com>
2019-06-01 14:29:00 +0800

    finish get schedule by user function

--- src/main/java/com/sxd/server/mytime/Controller/ScheduleController/GetScheduleController.java
+++ src/main/java/com/sxd/server/mytime/Controller/ScheduleController/GetScheduleController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class GetScheduleController {
     @Autowired
@@ -22,5 +25,17 @@ public class GetScheduleController {
             System.out.println(e);
         }
         return schedule;
+    }
+
+    //获取目标用户的所有日程信息
+    @RequestMapping(value = "getScheduleByUser",method = RequestMethod.GET)
+    public List<Schedule> getScheduleByUser(Integer userId){
+        List<Schedule> list=new ArrayList<>();
+        try{
+            list=getScheduleService.getScheduleByUser(userId);
+        }catch (Exception e){
+            System.out.println(e);
+        }
+        return list;
     }
 }
--- src/main/java/com/sxd/server/mytime/Service/ScheduleService/GetScheduleService.java
+++ src/main/java/com/sxd/server/mytime/Service/ScheduleService/GetScheduleService.java
@@ -4,6 +4,9 @@ import com.sxd.server.mytime.Dao.ScheduleMapper;
 import com.sxd.server.mytime.Entity.Schedule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.List;
 
 @Service
 public class GetScheduleService {
@@ -15,5 +18,15 @@ public class GetScheduleService {
         Schedule schedule=null;
         schedule=scheduleMapper.selectByPrimaryKey(scheduleId);
         return schedule;
+    }
+
+    //获取目标用户的所有日程
+    public List<Schedule> getScheduleByUser(Integer userId)throws Exception{
+        List<Schedule> list=null;
+        Example example=new Example(Schedule.class);
+        Example.Criteria criteria=example.createCriteria();
+        criteria.andEqualTo("userId",userId);
+        list=scheduleMapper.selectByExample(example);
+        return list;
     }
 }
